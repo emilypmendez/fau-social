afb95277677e848c39aa54a2ae00e628632c4b19
Emily Mendez <emilypmendez@gmail.com>
2023-06-14 09:08:44 -0400

    update form names (netlify)

--- src/components/SuggestionBox.js
+++ src/components/SuggestionBox.js
@@ -8,23 +8,23 @@ const SuggestionBox = () => {
                 <h1>Suggestion Box</h1>
                 <br/>
                 <center><p>Have an idea for this website? Let us know!</p></center>
-                <form name="contact" method="POST" id="contact-form" data-netlify="true">
+                <form name="contact" method="POST" id="contact-form" data-netlify="true" onSubmit="submit" >
                     <input type="hidden" name="form-name" value="contact" />
                     <div className="form-group">
                         <label for="name">Name</label>
-                        <input type="text" className="form-control" id="name" placeholder="Enter your name" />
+                        <input type="text" className="form-control" id="name" placeholder="Enter your name" name="name" />
                     </div>
                     <br/>
                     <div className="form-group">
                         <label for="email">Email address</label>
-                        <input type="email" className="form-control" id="email" placeholder="Enter your email" />
+                        <input type="email" className="form-control" id="email" placeholder="Enter your email" name="email" />
                         <p>
                             <br/>
                         <label>Your Role: 
                             <select name="role[]" multiple>
-                                <option value="student">Student</option>
-                                <option value="faculty">Faculty</option>
-                                <option value="other">Other</option>
+                                <option value="student" className="form-control" name="student" >Student</option>
+                                <option value="faculty" className="form-control" name="faculty" >Faculty</option>
+                                <option value="other" className="form-control" name="other" >Other</option>
                             </select>
                         </label>
                         </p>
@@ -32,7 +32,14 @@ const SuggestionBox = () => {
                     <br/>
                     <div className="form-group">
                         <label for="suggestion">Suggestion</label>
-                        <textarea className="form-control" id="suggestion" rows="3" placeholder="Enter your suggestion. Please be as descriptive as possible."></textarea>
+                        <textarea 
+                            className="form-control" 
+                            id="suggestion" 
+                            rows="3" 
+                            placeholder="Enter your suggestion. Please be as descriptive as possible." 
+                            name="message"
+                        >
+                        </textarea>
                     </div>
                     <br/>
                     <center>
