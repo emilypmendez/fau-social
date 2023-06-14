24385a57a8f46e24209dbebafab5cd90fe317f55
Emily Mendez <emilypmendez@gmail.com>
2023-06-13 20:44:41 -0400

    updated and enabled form submissions on the suggestion box

--- src/components/SuggestionBox.js
+++ src/components/SuggestionBox.js
@@ -8,7 +8,8 @@ const SuggestionBox = () => {
                 <h1>Suggestion Box</h1>
                 <br/>
                 <center><p>Have an idea for this website? Let us know!</p></center>
-                <form>
+                <form name="contact" method="post" data-netlify="true">
+                    <input type="hidden" name="form-name" value="contact" />
                     <div className="form-group">
                         <label for="name">Name</label>
                         <input type="text" className="form-control" id="name" placeholder="Enter your name" />
@@ -17,15 +18,25 @@ const SuggestionBox = () => {
                     <div className="form-group">
                         <label for="email">Email address</label>
                         <input type="email" className="form-control" id="email" placeholder="Enter your email" />
+                        <p>
+                            <br/>
+                        <label>Your Role: 
+                            <select name="role[]" multiple>
+                                <option value="student">Student</option>
+                                <option value="faculty">Faculty</option>
+                                <option value="other">Other</option>
+                            </select>
+                        </label>
+                        </p>
                     </div>
                     <br/>
                     <div className="form-group">
                         <label for="suggestion">Suggestion</label>
-                        <textarea className="form-control" id="suggestion" rows="3" placeholder="Enter your suggestion"></textarea>
+                        <textarea className="form-control" id="suggestion" rows="3" placeholder="Enter your suggestion. Please be as descriptive as possible."></textarea>
                     </div>
                     <br/>
                     <center>
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <button type="submit" className="btn btn-primary">Send</button>
                     </center>
                 </form>
             </div>
